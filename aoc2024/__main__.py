2cd794c8b8d38cf3638897dd1907989798f027d5
Ioannis Foukarakis <21593330+ifoukarakis@users.noreply.github.com>
2024-12-01 22:41:55 +0200

    Update runner and docs

--- aoc2024/__main__.py
+++ aoc2024/__main__.py
@@ -5,14 +5,20 @@ import importlib
 def main():
     parser = argparse.ArgumentParser(description="Advent of Code 2024")
     parser.add_argument("day", type=int, help="Day of the challenge")
+    parser.add_argument("--duck", help="Run duck db solution", action="store_true")
     args = parser.parse_args()
 
     if args.day < 0 or args.day > 25:
         print("day number must be between 1 and 25")
         exit()
 
-    sol = importlib.import_module(f"aoc2024.day{args.day}.solution")
-    print(f"Answer to day {args.day}\n")
+    if args.duck:
+        stack = "DuckDB"
+        sol = importlib.import_module(f"aoc2024.day{args.day}.duck")
+    else:
+        stack = "Python"
+        sol = importlib.import_module(f"aoc2024.day{args.day}.solution")
+    print(f"Answer to day {args.day} - Implemented using {stack}\n")
     sol.solution()
 
 
