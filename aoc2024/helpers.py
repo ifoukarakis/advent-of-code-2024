3115912a03b5519941d27d8cacc052f640242439
Ioannis Foukarakis <21593330+ifoukarakis@users.noreply.github.com>
2024-12-01 22:42:44 +0200

    Add duckdb solution for day 1 (#3)

--- aoc2024/helpers.py
+++ aoc2024/helpers.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from typing import List
 
+import duckdb
+
 DATA_DIR = Path(__file__).parent.parent / 'data'
 
 
@@ -12,3 +14,18 @@ def load_data(day: int) -> List[str]:
     """
     with open(DATA_DIR / f'day{day}' / 'input.txt') as f:
         return f.read().splitlines()
+
+
+def load_data_to_duckdb(day: int):
+    """
+    Load the data for a given day into a duckdb table. Assumes the data is in a file called input.txt in a folder
+    called data/day{day}. Table will be named raw_day_<day number> and has a single column, named 'line'.
+    :param day:
+    :return:
+    """
+    duckdb.sql(
+        f"""
+        CREATE TABLE raw_day_{day} AS
+            FROM read_csv('data/day{day}/input.txt', header=false, columns={{'line': 'VARCHAR'}})
+        """
+    )
