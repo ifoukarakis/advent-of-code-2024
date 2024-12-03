efcad2755bdb826bf82008a157cc29a1a6c19c76
Ioannis Foukarakis <21593330+ifoukarakis@users.noreply.github.com>
2024-12-03 20:10:41 +0200

    Day 3: DuckDB solution (#7)

--- aoc2024/helpers.py
+++ aoc2024/helpers.py
@@ -20,16 +20,25 @@ def load_data(day: int, as_single_string: bool = False) -> List[str] | str:
         return f.read().splitlines()
 
 
-def load_data_to_duckdb(day: int):
+def load_data_to_duckdb(day: int, as_single_string: bool = False):
     """
     Load the data for a given day into a duckdb table. Assumes the data is in a file called input.txt in a folder
     called data/day{day}. Table will be named raw_day_<day number> and has a single column, named 'line'.
-    :param day:
-    :return:
+    :param day: the day to load the data for.
+    :param as_single_string: if True, load to a new table with a single row containing the entire file as a single
+     string.
     """
-    duckdb.sql(
-        f"""
-        CREATE TABLE raw_day_{day} AS
-            FROM read_csv('data/day{day}/input.txt', header=false, columns={{'line': 'VARCHAR'}})
-        """
-    )
+    if as_single_string:
+        duckdb.sql(
+            f"""
+            CREATE TABLE raw_day_{day} AS
+                FROM read_blob('data/day{day}/input.txt')
+            """
+        )
+    else:
+        duckdb.sql(
+            f"""
+            CREATE TABLE raw_day_{day} AS
+                FROM read_csv('data/day{day}/input.txt', header=false, columns={{'line': 'VARCHAR'}})
+            """
+        )
