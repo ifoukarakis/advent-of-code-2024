a62a7342803707921a945a28118d02f793c298c9
Ioannis Foukarakis <21593330+ifoukarakis@users.noreply.github.com>
2024-12-06 00:50:18 +0200

    Day 5: duckdb part 2 (#13)
    * Day 5: duckdb part 2
    
    * Fix naming

--- aoc2024/day5/duck.py
+++ aoc2024/day5/duck.py
@@ -39,10 +39,10 @@ def preprocess():
         )
         select
             update_id,
-            string_split(updates, ',')::int[] as raw_update,
-            unnest(raw_update)::int as page,
-            generate_subscripts(raw_update, 1) as index,
-            index = (len(raw_update) + 1) // 2 as is_middle
+            string_split(updates, ',')::int[] as all_pages,
+            unnest(all_pages)::int as page,
+            generate_subscripts(all_pages, 1) as index,
+            index = (len(all_pages) + 1) // 2 as is_middle
         from lines
     """
     )
@@ -81,7 +81,43 @@ def solve_part1() -> int:
 
 
 def solve_part2() -> int:
-    pass
+    return duckdb.sql(
+        """
+        with update_validity_check as (
+            -- For each update, check if it is a violation by comparing each page with all other pages
+            select
+                a.update_id,
+                a.page as current_page,
+                a.index as current_index,
+                b.page as downstream_page,
+                b.index as downstream_index,
+                r.before is not null as is_violation,
+                sum(case when is_violation then 1 else 0 end) over (partition by a.update_id) = 0 as valid_update
+            from
+                updates a
+                join updates b on a.update_id = b.update_id
+                left join rules r on a.page = r.after and b.page = r.before
+            where
+                a.index < b.index
+            order by a.update_id, current_index, downstream_index
+        ), rearranged_updates as (
+            select
+                update_id,
+                all_pages,
+                page,
+                index as original_index,
+                count(rules.before) + 1 as new_index,
+                new_index = (len(all_pages) + 1) // 2 as is_middle
+            from
+                updates
+                left join rules on page = after and list_contains(all_pages, before)
+            where
+                update_id in (select update_id from update_validity_check where not valid_update)
+            group by 1, 2, 3, 4
+        )
+        select sum(page) from rearranged_updates where is_middle
+        """
+    )
 
 
 def solution():
